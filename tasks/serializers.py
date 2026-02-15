572a978f62f359399251c338671de5997bc39ef4
Aiqerim Roman <romanaigerim7@gmail.com>
2026-02-15 22:56:43 +0500

    feature: update serializer validation and create behavior

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskCreateSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(min_length=5)
+
     class Meta:
         model = Task
         fields = ["title", "is_done"]
--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,6 @@ from .serializers import TaskCreateSerializer
 class TaskCreateAPIView(CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(is_done=False)
